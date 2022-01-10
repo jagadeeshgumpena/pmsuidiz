b010db813fcecdeea2c773d50674dfacdff72c53
DIS Mobile developers <MOBILEDEV@dizitiveit>
2022-01-10 23:42:57 +0530

    otpsender changes

--- src/main/java/com/dizitiveit/pms/model/OtpSender.java
+++ src/main/java/com/dizitiveit/pms/model/OtpSender.java
@@ -1,162 +1,212 @@
-package com.dizitiveit.pms.service;
+package com.dizitiveit.pms.model;
 
-import java.util.Random;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import org.springframework.stereotype.Component;
+import java.io.BufferedReader; 
+import java.io.DataOutputStream; 
+import java.io.InputStreamReader; 
+import java.net.HttpURLConnection; 
+import java.net.URL; 
+import java.net.URLEncoder;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import javax.net.ssl.HostnameVerifier; 
+import javax.net.ssl.SSLSession;
 
-import com.dizitiveit.pms.model.OtpSender;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
-import com.google.common.cache.LoadingCache;
+@Component("otpSender")
+public class OtpSender {
 
-@Service
-public class OtpSenderService {
-	
-	@Autowired
-	private OtpSender otpsender;
+	 private String username;
+	  private String password; 
+	  private String message;
+	  private String type;
+	  private String dlr; 
+	  private String destination;
+	  private String source; 
+	  private String server;
+	  private String entityid;
+	  private String tempid;
+	  private int port;
+	  
+	  public OtpSender(String username, String password, String message, String  type, String dlr, String destination, String source, String server,String entityid,String tempid, int port)
+	  { 
+		  super(); 
+		  this.username = username; 
+		  this.password = password; 
+		  this.message = message; 
+		  this.type = type; 
+		  this.dlr = dlr; 
+		  this.destination = destination;
+	  this.source = source;
+	  this.server = server;
+	  this.entityid = entityid;
+	  this.tempid = tempid;	  
+	  this.port = port; 
+	  
+	  }
+	  
+	  
+	  
+	  public OtpSender() { // TODO Auto-generated constructor stub 
+		  }
+	  
+	  
+	  
+	  
+	  public String submitMessage() { 
+		  HttpURLConnection httpConnection = null;
+	  String message = null; 
+	  try { 
+		  // Url that will be called to submit the message
+		  System.out.println("username"+this.username+"password"+this.password);
+	  URL sendUrl = new URL("http://" + this.server + ":" + this.port + "/bulksms/bulksms");
+	  
+	  @SuppressWarnings("unused")
+	  HostnameVerifier hostVerfier = new HostnameVerifier() { 
+		  public boolean verify(String urlHostName, SSLSession session) { 
+			  return true; 
+			  } }; 
+			  trustAllHttpsCertificates(); 
+			  httpConnection = (java.net.HttpURLConnection) sendUrl.openConnection();
+	  httpConnection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.29 Safari/537.36");
+	  httpConnection.setRequestMethod("POST"); 
+	  httpConnection.setDoInput(true);
+	  httpConnection.setDoOutput(true);
+	  httpConnection.setUseCaches(false);
+	  
+	  DataOutputStream dataStreamToServer = new
+	  DataOutputStream(httpConnection.getOutputStream());
+	  dataStreamToServer.writeBytes("username=" + URLEncoder.encode(this.username,
+	  "UTF-8") + "&password=" + URLEncoder.encode(this.password, "UTF-8") +
+	  "&type=" + URLEncoder.encode(this.type, "UTF-8") + "&dlr=" +
+	  URLEncoder.encode(this.dlr, "UTF-8") + "&destination=" +
+	  URLEncoder.encode(this.destination, "UTF-8") + "&source=" +
+	  URLEncoder.encode(this.source, "UTF-8") + "&message=" +
+	  URLEncoder.encode(this.message, "UTF-8") + "&entityid=" +
+	  URLEncoder.encode(this.entityid, "UTF-8")+ "&tempid=" +
+	  URLEncoder.encode(this.tempid, "UTF-8")); 
+	  dataStreamToServer.flush();
+	  dataStreamToServer.close();
+	 
+	  
+	  BufferedReader dataStreamFromUrl = new BufferedReader( new InputStreamReader(httpConnection.getInputStream()));
+	  String dataFromUrl = "",
+	  dataBuffer = "";
+	  
+	  while ((dataBuffer = dataStreamFromUrl.readLine()) != null) {
+		  dataFromUrl += dataBuffer;
+		  }
+	  System.out.println(dataStreamFromUrl);
+	  dataStreamFromUrl.close();
+	  
+	  System.out.println("Response Success: " + dataFromUrl);
+	  
+	  message = dataFromUrl.substring(0, 4);
+	  } catch (Exception ex) {
+	  System.out.println("Response: " + ex.getMessage());
+	  System.out.println("error message - > " + ex.getMessage());
+	  ex.printStackTrace(); 
+	  } finally { 
+		  if (httpConnection != null) {
+	  httpConnection.disconnect(); } 
+		  } 
+	  return message; 
+	  }
+	  
+	  private static void trustAllHttpsCertificates() throws Exception {
+		  // Create  a trust manager that does not validate certificate chains:
+	  javax.net.ssl.TrustManager[] trustAllCerts = new
+	  javax.net.ssl.TrustManager[1]; javax.net.ssl.TrustManager tm = new miTM();
+	  
+	  trustAllCerts[0] = tm; javax.net.ssl.SSLContext sc =
+	  javax.net.ssl.SSLContext.getInstance("SSL"); sc.init(null, trustAllCerts, null);
+	  
+	  javax.net.ssl.HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	  
+	  }
+	  
+	  public static class miTM implements javax.net.ssl.TrustManager,
+	  javax.net.ssl.X509TrustManager { public java.security.cert.X509Certificate[]
+	  getAcceptedIssuers() { return null; }
+	  
+	  public boolean isServerTrusted(java.security.cert.X509Certificate[] certs) {
+	  return true; }
+	  
+	  public boolean isClientTrusted(java.security.cert.X509Certificate[] certs) {
+	  return true; }
+	  
+	  public void checkServerTrusted(java.security.cert.X509Certificate[] certs,
+	  String authType) throws java.security.cert.CertificateException { return; }
+	  
+	  public void checkClientTrusted(java.security.cert.X509Certificate[] certs,
+	  String authType) throws java.security.cert.CertificateException { return; } }
+	  
+	  public String getUsername() { return username; }
+	  
+	  public void setUsername(String username) { this.username = username; }
+	  
+	  public String getPassword() { return password; }
+	  
+	  @Override public String toString() { return "OtpSender [username=" + username
+	  + ", password=" + password + ", message=" + message + ", type=" + type +
+	  ", dlr=" + dlr + ", destination=" + destination + ", source=" + source +
+	  ", server=" + server + ",entityid=" + entityid +",tempid = " + tempid +", port=" + port + "]"; }
+	  
+	  
+	  
+	  public void setPassword(String password) { this.password = password; }
+	  
+	  public String getMessage() { return message; }
+	  
+	  public void setMessage(String message) { this.message = message; }
+	  
+	  public String getType() { return type; }
+	  
+	  public void setType(String type) { this.type = type; }
+	  
+	  public String getDlr() { return dlr; }
+	  
+	  public void setDlr(String dlr) { this.dlr = dlr; }
+	  
+	  public String getDestination() { return destination; }
+	  
+	  public void setDestination(String destination) { this.destination =
+	  destination; }
+	  
+	  public String getSource() { return source; }
+	  
+	  public void setSource(String source) { this.source = source; }
+	  
+	  public String getServer() { return server; }
+	  
+	  public void setServer(String server) { this.server = server; }
+	  
+	  public int getPort() { return port; }
+	  
+	  public void setPort(int port) { this.port = port; }
 
-	private static final Integer EXPIRE_MIN = 2;
-	private LoadingCache<String, Integer> otpCache;
 
-	@Value("${sms.sever}")
-	private String SMSSERVER;
 
-	@Value("${sms.port}")
-	private int SMSPORT;
-
-	@Value("${sms.username}")
-	private String SMSUSERNAME;
-
-	@Value("${sms.password}")
-	private String SMSPASSWORD;
-
-	@Value("${sms.source}")
-	private String SMSSOURCE;
-
-	@Value("${sms.dlr}")
-	private String SMSDLR;
-
-	@Value("${sms.type}")
-	private String SMSTYPE;
-	
-	@Value("${sms.entityid}")
-	private String ENTITYID;
-	
-	@Value("${sms.tempid}")
-	private String TEMPID;
-
-	private String otpNumber;
-
-	public String sendOtp(String mobile, String msg) {
-
-		otpNumber = String.valueOf(OTP(6));
-		String message = "<#> Your PMS OTP Code is"+otpNumber+" "+msg;
-
-		otpsender.setServer(SMSSERVER);
-		otpsender.setPort(SMSPORT);
-		otpsender.setUsername(SMSUSERNAME);
-		otpsender.setPassword(SMSPASSWORD);
-		otpsender.setMessage(convertToUnicode(message).toString());
-		otpsender.setDlr(SMSDLR);
-		otpsender.setType(SMSTYPE);
-		otpsender.setDestination(mobile);
-		otpsender.setSource(SMSSOURCE);
-		otpsender.setEntityid(ENTITYID);
-		otpsender.setTempid(TEMPID);
-
-		System.out.println("SMS DETAILS: " + otpsender.toString());
-		otpsender.submitMessage();
-		int i = Integer.parseInt(otpNumber);
-		otpCache.put(mobile, i);
-		try {
-			System.out.println("cache is" + otpCache.get(mobile));
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-		return otpNumber;
-
+	public String getEntityid() {
+		return entityid;
 	}
 
-	private static StringBuffer convertToUnicode(String regText) {
-		char[] chars = regText.toCharArray();
-		StringBuffer hexString = new StringBuffer();
-		for (int i = 0; i < chars.length; i++) {
-			String iniHexString = Integer.toHexString((int) chars[i]);
-			if (iniHexString.length() == 1) {
-				iniHexString = "000" + iniHexString;
-			} else if (iniHexString.length() == 2) {
-				iniHexString = "00" + iniHexString;
-			} else if (iniHexString.length() == 3) {
-				iniHexString = "0" + iniHexString;
-			}
-			hexString.append(iniHexString);
-		}
-		System.out.println(hexString);
-		return hexString;
+
+
+	public void setEntityid(String entityid) {
+		this.entityid = entityid;
 	}
 
-	static char[] OTP(int len) {
 
-		String numbers = "0123456789";
 
-		// Using random method
-		Random rndm_method = new Random();
-		char[] otp = new char[len];
-
-		for (int i = 0; i < len; i++) {
-			// use of charAt() method : to get character value
-			// use of nextInt() as it is scanning the value as int
-			otp[i] = numbers.charAt(rndm_method.nextInt(numbers.length()));
-		}
-		return otp;
+	public String getTempid() {
+		return tempid;
 	}
 
-	public OtpSenderService() {
-		super();
-		otpCache = CacheBuilder.newBuilder().expireAfterWrite(EXPIRE_MIN, TimeUnit.MINUTES)
-				.build(new CacheLoader<String, Integer>() {
-					public Integer load(String key) {
-						return 0;
-					}
-				});
-	}
 
-	public int getOtp(String key) {
-		try {
-			return otpCache.get(key);
-		} catch (Exception e) {
-			return 0;
-		}
-	}
 
-	public void clearOTP(String key) {
-		otpCache.invalidate(key);
-	}
-	
-	public String sendSms(String mobile, String msg) {
-
-		otpsender.setServer(SMSSERVER);
-		otpsender.setPort(SMSPORT);
-		otpsender.setUsername(SMSUSERNAME);
-		otpsender.setPassword(SMSPASSWORD);
-		otpsender.setMessage(convertToUnicode(msg).toString());
-		otpsender.setDlr(SMSDLR);
-		otpsender.setType(SMSTYPE);
-		otpsender.setDestination(mobile);
-		otpsender.setSource(SMSSOURCE);
-		otpsender.setEntityid(ENTITYID);
-		otpsender.setTempid(TEMPID);
-		otpsender.submitMessage();
-		
-
-		return "Sent";
-
-	}
+	public void setTempid(String tempid) {
+		this.tempid = tempid;
+	} 
+	  
+	  
 }
 
